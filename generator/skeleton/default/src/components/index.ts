f8005c3383f8aea08894488392cf75f6a786435e
ideaspace <foot_print@163.com>
2019-03-18 19:03:05 +0800

    feat: version 0.0.6

--- generator/skeleton/default/src/components/index.ts
+++ generator/skeleton/default/src/components/index.ts
@@ -1,24 +1,25 @@
 import {VueConstructor} from 'vue';
 import ViewContainer from './layout/Container.vue';
-import SpinLoader from './spin/SpinLoader.vue';
-import SvgIcon from './icon/SvgIcon.vue';
-import CountTo from './count/CountTo.vue';
-import TableTree from './table/TableTree.vue';
 
 interface CompsList {
   [key: string]: VueConstructor;
 }
 
-export const Comps: CompsList = {
+const Comps: CompsList = {
   ViewContainer,
-  SpinLoader,
-  SvgIcon,
-  CountTo,
-  TableTree,
 };
 
+const requireComps = require.context('@/components', true, /\.vue$/);
+
 const requireSvgIcon = require.context('@/assets/icons', false, /\.svg$/);
+
+requireComps.keys().forEach((filename) => {
+  const compName = filename.slice(filename.lastIndexOf('/') + 1, -4);
+  Comps[compName] = requireComps(filename).default;
+});
 
 requireSvgIcon.keys().forEach((fileName) => {
   requireSvgIcon(fileName);
 });
+
+export default Comps;
