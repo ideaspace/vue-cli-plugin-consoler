5fec4f481b02f04a04086132b119c9e1c2c4611b
ideaspace <foot_print@163.com>
2019-03-19 13:59:04 +0800

    fix: 更新模板帮助文档判断逻辑

--- generator/skeleton/default/src/components/layout/Header.vue
+++ generator/skeleton/default/src/components/layout/Header.vue
@@ -23,57 +23,63 @@
             </div>
           </div>
         </div>
+        <%_ if (options.helpLink) { _%>
         <div class="header-cont--panel">
           <el-dropdown @command="val => onMenuSelect('help', val)">
             <span class="header-cont--panel-help">
               <el-icon name="question"></el-icon>
             </span>
             <el-dropdown-menu slot="dropdown">
-              <el-dropdown-item command="help">帮助文档</el-dropdown-item>
+              <el-dropdown-item
+                command=""
+              >帮助文档</el-dropdown-item>
             </el-dropdown-menu>
           </el-dropdown>
         </div>
+        <%_ } _%>
       </div>
     </div>
   </div>
 </template>
 <script lang="ts">
-  import {Component, Vue} from 'vue-property-decorator';
-  import {postLogout} from '@/apis/auth';
+import { Component, Vue } from 'vue-property-decorator';
+import {postLogout} from '@/apis/auth';
 
-  @Component
-  export default class HeaderWrapper extends Vue {
-    public title = require('./../../app.config.ts').title;
-
-    public get username() {
-      return this.$store.state.user.username;
-    }
-
-    public onMenuSelect(type: string, val: string) {
-      if (type === 'action') {
-        if (val === 'logout') {
-          this.userLogout();
-        }
-        if (val === 'changePwd') {
-          this.$router.push({
-            name: 'user-account-password',
-          });
-        }
+@Component
+export default class HeaderWrapper extends Vue {
+  public title = require('./../../app.config.ts').title;
+  public get username() {
+    return this.$store.state.user.username;
+  }
+  public onMenuSelect(type:string, val: string) {
+    if (type === 'action') {
+      if (val === 'logout') {
+        this.userLogout();
+      }
+      if (val === 'changePwd') {
+        this.$router.push({
+          name: 'user-modify-password'
+        });
       }
     }
-
-    public async userLogout() {
-      try {
-        const r = await postLogout();
-        if (r.data.code === 0) {
-          this.$store.dispatch('menu/deleteMenuData');
-          this.$store.dispatch('user/deleteUserData');
-          location.href = '/#/auth/login';
-        }
-      } catch (e) {
+    <%_ if (options.helpLink) { _%>
+    if (type === "help") {
+      window.open(val, "_blank");
+    }
+    <%_ } _%>
+  }
+  public async userLogout() {
+    try {
+      const r = await postLogout();
+      if (r.data.code === 0) {
+        this.$store.dispatch('menu/deleteMenuData');
+        this.$store.dispatch('user/deleteUserData');
+        location.href = '/#/auth/login';
       }
+    } catch (e) {
     }
   }
+}
 </script>
 <style lang="scss" scoped>
 .header-wrapper {
@@ -88,16 +94,13 @@
   align-items: center;
   height: $top-bar-height;
   background-color: $aside-menu-bg-color;
-
   &--panel {
     padding-right: $base-ratio-px;
   }
-
   &--panel-wrapper {
     display: flex;
     align-items: center;
   }
-
   &--brand-wrapper {
     height: $top-bar-height;
     overflow: hidden;
@@ -105,7 +108,6 @@
     padding-left: 10px;
     line-height: $top-bar-height;
     font-size: 0;
-
     .brand-img {
       display: inline-block;
       background-image: url("../../assets/logo.svg");
@@ -125,19 +127,16 @@
       transition: all .1s ease;
     }
   }
-
   &--panel-help {
     cursor: pointer;
     display: flex;
     justify-content: center;
     align-items: center;
     color: #b3b5b6;
-
     &:hover {
       border-radius: 50%;
       background-color: #383f45;
     }
-
     @include square($base-ratio * 3)
   }
 }
